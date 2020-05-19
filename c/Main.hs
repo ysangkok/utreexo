4ced2a906647c9fbc011fef19613fcd3b5cf465c
Janus <ysangkok@gmail.com>
2020-05-19 01:16:36 -0500

    WIP: cgo: remTransPre

--- c/Main.hs
+++ c/Main.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE ForeignFunctionInterface #-}
+{-# LANGUAGE ForeignFunctionInterface, TypeApplications #-}
 
 import System.IO.Unsafe (unsafePerformIO)
 import Forest
@@ -11,14 +11,18 @@ import Foreign.C.String (peekCString)
 import Test.Tasty
 import Test.Tasty.HUnit
 
+import Data.Functor.Identity (Identity)
 import Data.Word (Word64)
 import Data.List (inits)
-
+import Data.List.Split (chunksOf)
+import Data.Maybe (fromMaybe, catMaybes)
+import Data.Either (lefts, rights)
 import Data.Bits
 
-import Control.Monad.State.Lazy (runState, modify, get)
-import Data.Maybe (fromMaybe)
-import Control.Monad (forM_)
+import Control.Monad.State.Lazy (runState, modify, get, put, lift, State)
+import Pipes.Prelude (toListM)
+import Pipes (yield, Producer)
+import Control.Monad (forM_, unless, mzero)
 
 import Debug.Trace
 
@@ -350,11 +354,196 @@ tests5 = testGroup "swapCollapses tests"
     , testCase "3" $ swapCollapses [[(13, 9), (18, 15), (24, 22), (28, 26), (36, 31)], [(72, 70), (79, 76)], [(102, 100)], [], [], []] [Nothing, Nothing, Nothing, Just (114, 114), Just (120, 120), Nothing] @?= [Nothing, Nothing, Nothing, Just (114, 114), Just (120, 120), Nothing]
   ]
 
+rootPosition :: Word64 -> Int -> Int -> Word64
+rootPosition leaves h forestRows =
+  let
+    mask = 2 `shiftL` forestRows - 1
+    before = leaves .&. (mask `shiftL` (h + 1))
+    shifted = (before `shiftR` h) .|. (mask `shiftL` (forestRows - (h - 1)))
+  in
+    shifted .&. mask
+
+tests6 :: TestTree
+tests6 = testGroup "rootPosition tests"
+  [
+      testCase "1" $ rootPosition 5 0 3 @?= 4
+    , testCase "2" $ rootPosition 5 1 3 @?= 10
+    , testCase "3" $ rootPosition 5 2 3 @?= 12
+    , testCase "4" $ rootPosition 29 0 5 @?= 28
+    , testCase "5" $ rootPosition 29 1 5 @?= 46
+    , testCase "6" $ rootPosition 29 2 5 @?= 54
+    , testCase "7" $ rootPosition 29 3 5 @?= 58
+    , testCase "8" $ rootPosition 29 4 5 @?= 60
+    , testCase "9" $ rootPosition 31 0 5 @?= 30
+    , testCase "a" $ rootPosition 31 1 5 @?= 46
+    , testCase "b" $ rootPosition 31 2 5 @?= 54
+    , testCase "c" $ rootPosition 31 3 5 @?= 58
+  ]
+
+makeSwaps :: [Word64] -> Bool -> Word64 -> [(Word64, Word64)]
+makeSwaps dels rootPresent rootPos =
+  let
+    convert [x,y] = Just (x,y)
+    convert (oddLast : []) =
+        if rootPresent
+            -- this is swapped and xor'd
+            -- relative to the go code
+            -- so that the map below stays
+            -- simple. this triggers on the
+            -- last element if the list has
+            -- an odd length. (delRemains)
+            then Just (oddLast, rootPos `xor` 1)
+            else Nothing
+    pairs :: [(Word64, Word64)]
+    pairs = catMaybes $ map convert $ chunksOf 2 dels
+    f (first, second) = (second `xor` 1, first)
+  in
+    map f pairs
+    
+
+tests7 :: TestTree
+tests7 = testGroup "makeSwaps tests"
+  [
+      testCase "0" $ makeSwaps [] False 60 @?= []
+    , testCase "1" $ makeSwaps [2, 7, 9, 15, 19, 24, 28, 31, 33, 35, 36, 41] False 42 @?= [(6, 2), (14, 9), (25, 19), (30, 28), (34, 33), (40, 36)]
+    , testCase "2" $ makeSwaps [72, 79, 81] False 84 @?= [(78, 72)]
+    , testCase "3" $ makeSwaps [97, 99, 104] False 106 @?= [(98, 97)]
+    , testCase "4" $ makeSwaps [113, 115] False 116 @?= [(114, 113)]
+    , testCase "5" $ makeSwaps [121] False 122 @?= []
+    , testCase "6" $ makeSwaps [] False 124 @?= []
+    , testCase "7" $ makeSwaps [5, 9, 10, 12, 17, 27] True 28 @?= [(8, 5), (13, 10), (26, 17)]
+    , testCase "8" $ makeSwaps [32, 35, 36, 41, 45] False 46 @?= [(34, 32), (40, 36)]
+    , testCase "9" $ makeSwaps [49, 51] False 54 @?= [(50, 49)]
+    , testCase "a" $ makeSwaps [57] False 58 @?= []
+    , testCase "b" $ makeSwaps [] False 60 @?= []
+    , testCase "c" $ makeSwaps [2, 7, 9, 11] False 12 @?= [(6, 2), (10, 9)]
+  ]
+
+makeCollapse :: [Word64] -> Bool -> Int -> Word64 -> Word64 -> Int -> Maybe (Word64, Word64)
+makeCollapse dels rootPresent row numLeaves nextNumLeaves forestRows =
+  let
+    delRemains = length dels .&. 1 /= 0
+    rootDest = rootPosition nextNumLeaves row forestRows
+  in
+    case (delRemains, rootPresent) of
+      (False, True) ->
+        let rootSrc = rootPosition numLeaves row forestRows
+        in Just (rootSrc, rootDest)
+      (True, False) ->
+        let rootSrc = 1 `xor` (head $ reverse dels)
+        in Just (rootSrc, rootDest)
+      _ ->
+        Nothing
+
+tests8 :: TestTree
+tests8 = testGroup "makeCollapse tests"
+  [
+      testCase "0" $ makeCollapse [0, 4, 9, 12, 25, 26] True 0 29 13 5 @?= Just (28, 12)
+    , testCase "1" $ makeCollapse [37, 41, 43, 45] False 1 29 13 5 @?= Nothing
+    , testCase "2" $ makeCollapse [49, 51, 52] False 2 29 13 5 @?= Just (53, 50)
+    , testCase "3" $ makeCollapse [57] False 3 29 13 5 @?= Just (56, 56)
+    , testCase "4" $ makeCollapse [] False 4 29 13 5 @?= Nothing
+    , testCase "5" $ makeCollapse [3, 6, 14, 17, 19, 20] True 0 31 17 5 @?= Just (30, 16)
+    , testCase "6" $ makeCollapse [35, 37, 40] True 1 31 17 5 @?= Nothing
+    , testCase "7" $ makeCollapse [50, 53] False 2 31 17 5 @?= Nothing
+    , testCase "8" $ makeCollapse [] False 3 31 17 5 @?= Nothing
+    , testCase "9" $ makeCollapse [0, 3, 4] False 0 8 5 3 @?= Just (5, 4)
+    , testCase "a" $ makeCollapse [9, 10] False 1 8 5 3 @?= Nothing
+    , testCase "b" $ makeCollapse [13] False 2 8 5 3 @?= Just (12, 12)
+  ]
+
+makeSwapNextDels :: [Word64] -> Bool -> Int -> [Word64]
+makeSwapNextDels dels rootPresent forestRows =
+  let
+    convert [x,y] = Just $ parent y forestRows
+    convert (oddOne : []) =
+      if not rootPresent
+        then Just $ parent oddOne forestRows
+        else Nothing
+  in
+    catMaybes $ map convert $ chunksOf 2 dels
+
+tests9 :: TestTree
+tests9 = testGroup "makeSwapNextDels tests"
+  [
+      testCase "0" $ makeSwapNextDels [8, 11, 13, 14, 17, 18, 20] False 5 @?= [37, 39, 41, 42]
+    , testCase "1" $ makeSwapNextDels [32, 35, 37, 39, 41, 44] False 5 @?= [49, 51, 54]
+    , testCase "2" $ makeSwapNextDels [49, 51, 53] False 5 @?= [57, 58]
+    , testCase "3" $ makeSwapNextDels [] False 5 @?= []
+    , testCase "4" $ makeSwapNextDels [4, 10, 12, 14, 19, 24, 26, 28] True 5 @?= [37, 39, 44, 46]
+    , testCase "5" $ makeSwapNextDels [35, 37, 39, 44] False 5 @?= [50, 54]
+    , testCase "6" $ makeSwapNextDels [48, 50] False 5 @?= [57]
+    , testCase "7" $ makeSwapNextDels [0, 3, 4] False 3 @?= [9, 10]
+    , testCase "8" $ makeSwapNextDels [9, 10] False 3 @?= [13]
+    , testCase "9" $ makeSwapNextDels [13] False 3 @?= [14]
+    , testCase "a" $ makeSwapNextDels [57] False 5 @?= [60]
+    , testCase "b" $ makeSwapNextDels [57] True 5 @?= []
+  ]
+
+merge :: Ord a => [a] -> [a] -> [a]
+merge (x:xs) (y:ys) = if x < y
+                        then x:(merge xs (y:ys))
+                        else y:(merge (x:xs) ys)
+merge [] xs = xs
+merge xs [] = xs
+
+remTransPre :: [Word64] -> Word64 -> Int -> ([[(Word64, Word64)]], [Maybe (Word64, Word64)])
+remTransPre dels numLeaves forestRows =
+  let
+    nextNumLeaves = numLeaves - (fromInteger $ toInteger $ length dels)
+    producer :: Producer (Either [(Word64, Word64)] (Maybe (Word64, Word64))) (State [Word64]) ()
+    producer =
+      forM_ [0 .. forestRows - 1] $ \row -> do
+        readDels <- lift $ get
+        unless (length readDels == 0) $ do
+          let preRootPresent = numLeaves .&. (1 `shiftL` row) /= 0
+          let rootPos = rootPosition numLeaves row forestRows
+          let (gottenDels, rootPresent) = if preRootPresent && last readDels == rootPos
+                                              then (reverse $ tail $ reverse readDels, False)
+                                              else (readDels, preRootPresent)
+          let (twinNextDels, newDels) = extractTwins gottenDels forestRows
+          let swapNextDels = makeSwapNextDels newDels rootPresent forestRows
+          lift $ put $ merge twinNextDels swapNextDels
+          let collapsed = makeCollapse newDels rootPresent row numLeaves nextNumLeaves forestRows
+          let swaps = makeSwaps newDels rootPresent rootPos
+          yield $ Left swaps
+          yield $ Right collapsed
+    both = fst $ runState (toListM producer) dels
+    padding1 = replicate (forestRows - length (lefts both)) mzero
+    padding2 = replicate (forestRows - length (lefts both)) mzero
+  in
+    (lefts both ++ padding1, rights both ++ padding2)
+
+testsA :: TestTree
+testsA = testGroup "remTransPre tests"
+  [
+      testCase "0" $ remTransPre [1, 2, 7, 11, 12, 13, 14, 15, 16, 17, 20, 22, 23, 24, 25, 27, 28, 37, 40, 41, 42] 43 6 @?= ([[(3, 1), (10, 7), (26, 20), (36, 28)], [(68, 65), (74, 72)], [(103, 101)], [(114, 113)], [], []], [Nothing, Just (83, 74), Just (104, 100), Nothing, Just (120, 120), Nothing])
+    , testCase "1" $ remTransPre [] 0 5 @?= (replicate 5 [], [Nothing, Nothing, Nothing, Nothing, Nothing])
+    , testCase "2" $ remTransPre [0, 1, 2, 3, 4, 6, 12, 13, 15, 17, 18, 19, 20, 22, 24, 26, 27, 28] 29 5 @?= ([[(7, 4), (16, 15), (23, 20)], [(39, 35)], [(50, 48)], [], []], [Just (25, 10), Just (42, 36), Nothing, Just (56, 56), Nothing])
+    , testCase "3" $ remTransPre [] 0 4 @?= (replicate 4 [], replicate 4 Nothing)
+    , testCase "4" $ remTransPre [2, 3, 5, 8, 10, 11] 12 4 @?= ([[(9, 5)], [], [], []], [Nothing, Just (16, 18), Just (25, 24), Nothing])
+    , testCase "5" $ remTransPre [] 0 5 @?= (replicate 5 [], replicate 5 Nothing)
+    , testCase "6" $ remTransPre [0, 1, 3, 4, 6, 8, 9, 11, 12, 13, 14, 15, 18, 19, 22, 25, 26, 27] 28 5 @?= ([[(5, 3), (10, 6), (24, 22)], [(35, 32)], [(53, 49)], [], []], [Nothing, Just (40, 36), Nothing, Just (56, 56), Nothing])
+    , testCase "7" $ remTransPre [] 0 5 @?= (replicate 5 [], replicate 5 Nothing)
+    , testCase "8" $ remTransPre [1, 3, 5, 8, 9, 10, 12] 18 5 @?= ([[(2, 1), (11, 5)], [(39, 33)], [], [], []], [Just (13, 10), Just (40, 36), Nothing, Just (56, 56), Nothing])
+    , testCase "9" $ remTransPre [] 0 3 @?= (replicate 3 [], replicate 3 Nothing)
+    , testCase "a" $ remTransPre [0, 2, 3] 5 3 @?= ([[(4, 0)], [], []], [Nothing, Just (8, 8), Nothing])
+    , testCase "b" $ remTransPre [] 0 5 @?= (replicate 5 [], replicate 5 Nothing)
+    , testCase "c" $ remTransPre [3, 4, 6, 7, 11, 12, 13, 15, 16, 17, 18, 19, 20, 24, 27, 28] 29 5 @?= ([[(5, 3), (14, 11), (25, 20)], [], [(50, 49)], [], []], [Just (26, 12), Nothing, Just (53, 50), Just (56, 56), Nothing])
+    , testCase "d" $ remTransPre [] 0 5 @?= (replicate 5 [], replicate 5 Nothing)
+    , testCase "e" $ remTransPre [1, 3, 5, 6, 7, 9, 11, 17, 20, 25, 26, 28, 29, 30] 31 5 @?= ([[(2, 1), (8, 5), (16, 11), (24, 20)], [(34, 33), (41, 36)], [(53, 49)], [], []], [Just (27, 16)] ++ replicate 4 Nothing)
+    , testCase "f" $ remTransPre [] 0 3 @?= (replicate 3 [], replicate 3 Nothing)
+    , testCase "g" $ remTransPre [0, 3, 4] 8 3 @?= ([[(2, 0)], [(11, 9)], []], [Just (5, 4), Nothing, Just (12, 12)])
+  ]
+
 -- TODO remTrans2
 
 main :: IO ()
 main = do
-    defaultMain $ testGroup "all tests" [tests, tests2, tests3, tests4, tests5]
+    defaultMain $ testGroup "all tests" $
+        [
+          tests, tests2, tests3, tests4, tests5, tests6, tests7, tests8, tests9, testsA
+        ]
 
     putStrLn "HASKELL MAIN STARTS"
     let Just forest = forestWithLeaves testLeaves
