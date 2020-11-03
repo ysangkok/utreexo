f852cf66b3cb8ea53425e4090099aa24653fc8fa
Janus <ysangkok@gmail.com>
2020-11-03 15:25:11 -0600

    Simplify Anim with Lenses

--- c/Anim.hs
+++ c/Anim.hs
@@ -3,85 +3,108 @@ import GoImplFunctions (f21)
 import Lib (cbTreeToBTree, CBTree)
 import Forest (CLeaf, cleafToText)
 
-import Prelude hiding (lookup)
 import Data.Maybe (fromMaybe, catMaybes)
-import qualified Data.Tree
-import qualified Data.Map as Map
---import qualified Data.Text as T
-import Data.Function((&))
+import Data.Tree (Forest, Tree, rootLabel)
 import Data.Tree.Lens (branches)
-import Control.Lens.Operators ((.~), (^?)) -- <&>
-import Control.Lens.At (ix)
-import Data.List (inits)
+import Data.Function ((&))
+import Data.Semigroup.Foldable (foldMap1)
+import Data.Semigroup (Max(Max), Min(Min))
+import Data.List.NonEmpty (NonEmpty, fromList)
 
-import Diagrams.TwoD (scale)
-import Diagrams.TwoD.Types (P2, unp2, mkP2)
+import Control.Lens.Operators ((.~), (^?), (^..), (%~)) -- <&>
+import Control.Lens.At (ix)
+import Control.Lens (each, _2, folded)
+import Control.Arrow ((&&&))
+--import Data.Profunctor.Optic.Fold1 (folded1)
+
+import Diagrams.TwoD (scale, translateX)
+import Diagrams.TwoD.Types (P2, unp2, mkP2, _x)
 import Diagrams.TwoD.Layout.Tree (uniqueXLayout)
-import Reanimate (scene, fromToS, tweenVar, reanimate, mkBackground, simpleVar, addStatic, translate)
+import Reanimate (scene, fromToS, tweenVar, reanimate, mkBackground, simpleVar, addStatic, translate, Duration, pauseAtEnd)
 import qualified Reanimate
 import Reanimate.Animation(Animation)
 import Reanimate.Svg.Constructors (mkText, mkGroup)
 import qualified Graphics.SvgTree.Types as SVGT
-import Data.Semigroup (Max(Max), Min(Min))
 
+-- Formulas derived from trial & error and visual inspection
 nodeCoord :: P2 Double -> P2 Double
-nodeCoord p2 = mkP2 (x/5 - 7) (-y/2-2) where (x,y) = unp2 p2
+nodeCoord p2 = mkP2 (x/5 - 7) (-y/2 - 2) where (x,y) = unp2 p2
+
+-- Given a tree and an X-offset, offset all nodes in the tree by the X-offset
+treeAndOffsetToOffsetTree :: (Tree (CLeaf, P2 Double), Double) -> Tree (CLeaf, P2 Double)
+treeAndOffsetToOffsetTree (tree, offset) = (fmap.fmap) (translateX offset) tree
+
+-- Draw a single node, given its position
+toSVGElement :: CLeaf -> P2 Double -> SVGT.Tree
+toSVGElement lea p2 =
+    translate x y $ Reanimate.scale 0.08 $ mkText (cleafToText lea) & SVGT.fontFamily .~ pure ["Ubuntu Mono"]
+  where
+    (x,y) = unp2 $ nodeCoord p2
+
+-- The max X coord in the tree minus the min X coord in the tree, plus some padding
+oneWidth :: Tree (CLeaf, P2 Double) -> Double
+oneWidth t =
+  let
+    xs :: NonEmpty Double
+    xs = fromList $ t ^.. folded . _2 . _x -- bad because fromList is partial
+    -- xs = toNonEmptyOf (folded1 . _2 . _x) t -- requires profunctor-optics (broken?)
+    (Min minX, Max maxX) = foldMap1 (Min &&& Max) xs
+  in
+    maxX - minX + 5
+
+-- Given a forest, find the given sub-tree and offset it by the given vector, and draw them all
+svgT :: Forest (CLeaf, P2 Double) -> Tree CLeaf -> P2 Double -> [SVGT.Tree]
+svgT xOffsetTrees leavesToAnimate dest =
+  [ toSVGElement leaf newPos
+    | (leaf, pos) <- xOffsetTrees ^.. each . each
+    , let newPos = if leaf `elem` leavesToAnimate
+                     then pos+dest
+                     else pos
+  ]
 
 mkAnim :: [CBTree CLeaf] -> Animation
 mkAnim f =
-    let
-        layoutedTrees :: [Data.Tree.Tree (CLeaf, P2 Double)]
-        layoutedTrees = catMaybes $ map (uniqueXLayout 2 2 . cbTreeToBTree) f
-        toSVGElement :: CLeaf -> P2 Double -> SVGT.Tree
-        toSVGElement lea p2 =
-              translate x y $ Reanimate.scale 0.08 $ mkText (cleafToText lea) & SVGT.fontFamily .~ pure ["Ubuntu Mono"]
-            where
-              (x,y) = unp2 $ nodeCoord p2
-        oneWidth t =
-          let
-            yMinMax (_lea, p2) =
-              let
-                (x, _) = unp2 p2
-              in
-                Just (Min x, Max x)
-            Just (Min minX, Max maxX) = foldMap yMinMax t
-          in
-            maxX - minX + 1
-        widths = [x+4 | x <- map oneWidth layoutedTrees]
-        treesWithOffsets :: [(Data.Tree.Tree (CLeaf, P2 Double), Double)]
-        treesWithOffsets = zip layoutedTrees offsets
-        treeAndOffsetToOffsetTree :: (Data.Tree.Tree (CLeaf, P2 Double), Double) -> Data.Tree.Tree (CLeaf, P2 Double)
-        treeAndOffsetToOffsetTree (tree, offset) = fmap (\(x,y) -> (x, y + mkP2 offset 0)) tree
-        offsetTrees = map treeAndOffsetToOffsetTree treesWithOffsets
-        toLeafPosMap = Map.fromList.Data.Tree.flatten
-        eachTreeMoved :: [Map.Map CLeaf (P2 Double)]
-        eachTreeMoved = map toLeafPosMap offsetTrees
-        leafToPos :: Map.Map CLeaf (P2 Double)
-        leafToPos = Map.unions eachTreeMoved
-        offsets :: [Double]
-        offsets = reverse $ tail $ reverse $ map sum $ inits widths
-        svgT :: Data.Tree.Tree (CLeaf, P2 Double) -> P2 Double -> [SVGT.Tree]
-        svgT leavesWithCoords dest = let
-                onlyCLeaves :: [CLeaf]
-                onlyCLeaves = map fst $ Data.Tree.flatten leavesWithCoords
-              in
-                [svgElem
-                  | (leaf, pos) <- Map.toList leafToPos
-                  , let newPos = if leaf `elem` onlyCLeaves then pos+dest else pos
-                  , let svgElem = toSVGElement leaf newPos]
-        tweenForest :: Double -> SVGT.Tree
-        tweenForest v = let
-            fromTree = fromMaybe (error "err1") $ offsetTrees ^? ix 2 . branches . ix 1
-            destTree = fromMaybe (error "err2") $ offsetTrees ^? ix 3 . branches . ix 0 . branches . ix 1
-            (_,        fromPos) = head $ foldMap pure fromTree
-            (_,        destPos) = head $ foldMap pure destTree
-            scaled = scale v (destPos - fromPos)
-          in
-            mkGroup $ svgT fromTree scaled
-    in
-        addStatic (mkBackground "white") $ scene $
-          do v <- simpleVar tweenForest 0.0001
-             tweenVar v 1 $ \val -> fromToS val 1 -- from 0 to 1
+  let
+    layoutedTrees :: Forest (CLeaf, P2 Double)
+    layoutedTrees = catMaybes $ map (uniqueXLayout 2 2 . cbTreeToBTree) f
+
+    -- Widths from left to right
+    widths = map oneWidth layoutedTrees
+
+    -- Offsets for every tree such that they do not overlap. Offset every tree by the widths of the trees to the left of it.
+    -- This will be one longer than amount of trees, but zip below will just ignore that last element.
+    xOffsets :: [Double]
+    xOffsets = 0 : scanl1 (+) widths
+
+    -- Move all nodes in each tree according to the X-offset corresponding to the tree
+    xOffsetTrees :: Forest (CLeaf, P2 Double)
+    xOffsetTrees = zip layoutedTrees xOffsets & each %~ treeAndOffsetToOffsetTree
+
+    fromTree = fromMaybe (error "tree to move not found")            $ xOffsetTrees ^? ix 2 . branches . ix 1
+    destTree = fromMaybe (error "tree getting moved onto not found") $ xOffsetTrees ^? ix 3 . branches . ix 0 . branches . ix 1
+    (_, fromPos) = rootLabel fromTree
+    (_, destPos) = rootLabel destTree
+
+    -- `fraction` is the progress of the nodes to move:
+    --    0 means they should be unmoved (still at fromPos)
+    --    1 means they should be moved all the way to to destPos
+    tweenForest :: Double -> SVGT.Tree
+    tweenForest fraction =
+      let
+        scaled = scale fraction (destPos - fromPos)
+        onlyCLeaves = fmap fst fromTree
+      in
+        mkGroup $ svgT xOffsetTrees onlyCLeaves scaled
+  in
+    scene (
+      do v <- simpleVar tweenForest 0.0001
+         let duration = 1 :: Duration
+         -- Without flip, the tween would go 1->0. But we want 0->1.
+         tweenVar v duration (flip fromToS 1)
+    )
+      & addStatic (mkBackground "white")
+      & pauseAtEnd 0.5
+
 
 main :: IO ()
 main = reanimate (mkAnim f21)
