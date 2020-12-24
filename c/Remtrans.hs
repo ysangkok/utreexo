7eefc2e59daa56addc305a2218f596a05a988061
Janus <ysangkok@gmail.com>
2020-12-23 18:19:57 -0600

    Remtrans: Verify that deletion wipes chosen element
    - Enable deleting multiple nodes leaves in one batch
    - Remove biggest subtree across all trees in forest
    - Use MonadFail instead of `error`
    - Prettier tree printing for debugging

--- c/Remtrans.hs
+++ c/Remtrans.hs
@@ -8,6 +8,7 @@
 {-# LANGUAGE RankNTypes #-}
 module Remtrans where
 
+import Debug.Trace (traceM)
 import Data.Monoid
 import Data.Ord (comparing, Down(Down))
 import Control.Monad.State.Lazy
@@ -20,31 +21,29 @@ import Control.Lens hiding (Empty, index, filtered)
 import qualified Data.Tree as D
 import Data.Bits
 import Data.List hiding (break, last)
+import Data.Tree.Pretty
 
 
 
 data Tree a = Node {_val :: a, _left :: (Tree a), _right :: (Tree a)} | Empty
-  deriving (Show, Eq, Functor, Foldable, Traversable)
+  deriving (Show, Eq, Functor, Foldable, Traversable, Ord)
 
-
-toDat :: (Tree a) -> (D.Tree (Maybe a))
-toDat Empty = D.Node Nothing []
-toDat (Node v a b) = D.Node (Just v) [toDat a, toDat b]
+toDat :: Show a => [Tree a] -> [D.Tree String]
+toDat forest =
+  fmap go forest
+  where
+  go :: Show a => Tree a -> D.Tree String
+  go Empty = D.Node "empty" [] -- while moving out of a forest, we can set the top node to Empty
+  go (Node v Empty Empty) = 
+    D.Node (show v) []
+  go (Node v Empty b) =
+    D.Node (show v) ([D.Node "cut" [], go b])
+  go (Node v a Empty) =
+    D.Node (show v) ([go a, D.Node "cut" []])
+  go (Node v a b) = 
+    D.Node (show v) ([go a, go b])
 
 makeLenses ''Tree
-
--- could be built from HasSiblings
---  [Node (6,False)
---    (Node (4,False)
---      (Node (0,False) Empty Empty)
---      (Node (1,False) Empty Empty))
---    (Node (5,False)
---      (Node (2,False) Empty Empty)
---      (Node (3,False) Empty Empty))]
-    
-    
-
-
 
 hasNoProof :: Foldable t => t (a, Bool) -> Bool
 hasNoProof = all (not.snd)
@@ -52,7 +51,7 @@ hasNoProof = all (not.snd)
 
 removePerfectSubtreeWithNoProof :: [Tree (a, Bool)] -> Maybe (Tree a, [Tree (a, Bool)])
 removePerfectSubtreeWithNoProof forest = do
-  idx <- findInForest (\(_,x) -> if hasNoProof $ leavesOnly x then perfectDepth x else Nothing) forest
+  idx <- findInForest (\(_,x) -> if hasNoProof $ leavesOnly x then fmap Down (perfectDepth x) else Nothing) forest
   let foundNoProof = idxToLens idx
   let foundNoProof2 = idxToLens idx
   return $ (fmap fst $ forest ^. foundNoProof, forest & foundNoProof2 .~ Empty)
@@ -91,25 +90,20 @@ treeHasOnlyNothing a = length (catMaybes (leavesOnly a)) == 0
 
 -- takes the sub-tree pointed to by findPerfectSubtree
 --   and partially filled out newStructure
-copyLeaves :: forall a. Eq a => Tree a -> [Tree (Maybe a)] -> [Tree (Maybe a)]
+copyLeaves :: forall a m. (Eq a, MonadFail m) => Tree a -> [Tree (Maybe a)] -> m [Tree (Maybe a)]
 copyLeaves perfectSubtree newStructure =
   let
   inserted :: Tree (Maybe a)
   inserted = fmap Just perfectSubtree
   in
-  case findInForest (\(_,x) -> if treeHasOnlyNothing x then perfectDepth x else Nothing) newStructure of 
+  case findInForest (\(_,x) -> if treeHasOnlyNothing x && perfectDepth x == perfectDepth perfectSubtree then perfectDepth x else Nothing) newStructure of 
     Just idx ->
       let
       lensToSubtreeWithEmptyLeaves = idxToLens idx
       in
-      newStructure & lensToSubtreeWithEmptyLeaves .~ inserted
+      return $ newStructure & lensToSubtreeWithEmptyLeaves .~ inserted
     Nothing ->
-      error "could not find destination"
-  --[ Node (Just 5)
-  --    (Node (Just 2) Empty Empty)
-  --    (Node (Just 3) Empty Empty)
-  --, Node Nothing Empty Empty
-  --]
+      fail "could not find destination"
 
 -- take an almost filled tree, fill out the remaining nodes, remove the Maybe
 fillRemaining :: [Tree (Maybe a)] -> [a] -> [Tree (Maybe a)]
@@ -128,15 +122,16 @@ fillRemaining forest (newVal: newVals) =
 compose :: (Functor t, Foldable t) => t (b -> b) -> b -> b
 compose = appEndo . foldl1 (<>) . fmap Endo
 
-prog :: forall a m. (Monad m, Show a, Eq a) => [Tree a] -> (Int, [Int]) -> (String -> m ()) -> m [Tree a]
-prog perfectStructure delPath traceM =
+
+prog :: forall m a. (MonadFail m, Show a, Eq a) => [Tree a] -> [(Int, [Int])] -> (String -> m ()) -> m [Tree (Maybe a)]
+prog perfectStructure dels traceM =
   let
     oldStructure :: [Tree (a, Bool)]
     oldStructure =
        fmap (fmap (, False)) perfectStructure
-         & idxToLens delPath .~ Empty
+         & (compose [idxToLens del .~ Empty | del <- dels])
     delWithSiblings :: [(Int, [Int])]
-    delWithSiblings = tuplToSiblings delPath
+    delWithSiblings = join $ map tuplToSiblings dels
       where
       tuplToSiblings (root, idxs) = [(root, reverse $ last `xor` 1 : previous) | last : previous <- map reverse $ tail $ inits idxs]
                                  ++ [(root,                          previous) |        previous <-                      inits idxs]
@@ -144,27 +139,34 @@ prog perfectStructure delPath traceM =
     oldStructureWithHaveProof = (compose siblingSetters) oldStructure
     siblingSetters :: [[Tree (a, Bool)] -> [Tree (a, Bool)]]
     siblingSetters = [idxToLens i . val . _2 .~ True | i <- delWithSiblings]
-    Just depth = perfectDepth (perfectStructure !! fst delPath)
-    -- always perfect, inferrable from nextNumLeaves (powers of two)
+    leafCountSum = sum $ map (length.leavesOnly) perfectStructure
     newStructure :: [Tree (Maybe a)]
-    newStructure = emptyForest (2 ^ (depth - 1) - 1) Nothing -- just delete one
+    newStructure = emptyForest (leafCountSum - length dels) Nothing
     lift2 = lift . lift
-  in
-  flip evalStateT (oldStructureWithHaveProof, newStructure) $ 
+    drawForestM :: (Show b, MonadFail m) => [Tree b] -> m ()
+    drawForestM removedFrom =
+      let
+      datatrees = toDat removedFrom
+      in
+      traceM (drawVerticalForest datatrees)
+  in do
+  traceM "initial forest"
+  drawForestM oldStructureWithHaveProof
+  flip evalStateT (oldStructureWithHaveProof, newStructure) $
     loop $ do
-      (old, new) <- lift get
+      (old :: [Tree (a, Bool)], new :: [Tree (Maybe a)]) <- lift get
       let mlens = removePerfectSubtreeWithNoProof old
       case mlens of
         Just (subtree, removedFrom) -> do
           lift2 $ traceM "removed tree"
-          lift2 $ traceM (D.drawTree $ fmap show (toDat subtree))
+          lift2 $ drawForestM [subtree]
           lift2 $ traceM "forest after removal"
-          lift2 $ traceM (D.drawForest $ fmap (\x -> fmap show (toDat x)) removedFrom)
+          lift2 $ drawForestM removedFrom
           lift2 $ traceM "before insertion"
-          lift2 $ traceM (D.drawForest $ fmap (\x -> fmap show (toDat x)) new)
-          let inserted = copyLeaves subtree new
+          lift2 $ drawForestM new
+          inserted <- lift2 $ copyLeaves subtree new
           lift2 $ traceM "inserted"
-          lift2 $ traceM (D.drawForest $ fmap (\x -> fmap show (toDat x)) inserted)
+          lift2 $ drawForestM inserted
           lift $ put (removedFrom, inserted)
         Nothing -> do
           lift2 $ traceM "ran out of perfect subtrees with no proof, leaves:"
@@ -173,10 +175,10 @@ prog perfectStructure delPath traceM =
           lift2 $ traceM $ show leaves
           lift2 $ traceM "final, filled with remaining leaves"
           let final :: [Tree (Maybe a)] = fillRemaining new leaves
-          lift2 $ traceM (D.drawForest $ fmap (\x -> fmap show (toDat x)) final)
-          let noMaybe = fmap (\x -> fmap fromJust x) final
-          lift2 $ traceM (D.drawForest $ fmap (\x -> fmap show (toDat x)) noMaybe)
-          break (noMaybe)
+          lift2 $ drawForestM final
+          --let noMaybe = fmap (\x -> fmap fromJust x) final
+          --lift2 $ traceM (D.drawForest $ fmap (\x -> fmap show (toDat x)) noMaybe)
+          break final
 
 
 perfectDepth :: Tree a -> Maybe Int
@@ -210,31 +212,16 @@ findSubtrees _ _ Empty = []
 
 
 findInForest :: Ord o => (([Int], Tree a) -> Maybe o) -> [Tree a] -> Maybe (Int, [Int])
-findInForest ordering forest = listToMaybe $ do
-  (idx, tree) <- zip [0..] forest
-  (path, _) <- findSubtrees ordering [] tree
-  return $ (idx, path)
+findInForest ordering forest =
+  -- sort on the o, then discard it
+  listToMaybe $ map snd $ sortOn (ordering.fst) $ do
+    (idx, tree) <- zip [0..] forest
+    -- the o is just for ordering
+    o@(path, _) <- findSubtrees ordering [] tree
+    return $ (o, (idx, path))
 
---oldStructureWithHaveProof =
---    [Node (6,True)
---      (Node (4,True)
---        Empty
---        (Node (1,True) Empty Empty))
---      (Node (5,True)
---        (Node (2,False) Empty Empty)
---        (Node (3,False) Empty Empty))]
-
---  print $
---    newStructure @()
---   ==
---    [ Node Nothing
---        (Node Nothing Empty Empty)
---        (Node Nothing Empty Empty)
---    , Node Nothing Empty Empty
---    ]
-
-number :: Tree () -> Tree Int
-number inp = fst $ runState (traverse fun inp) 0
+number :: [Tree ()] -> [Tree Int]
+number inp = fst $ runState (traverse (traverse fun) inp) 0
   where
     fun _ = do { x <- get; modify (+1); return x}
 
@@ -242,11 +229,11 @@ main :: IO ()
 main = do
   --print $ (join $ traverse leavesOnly oldStructureWithHaveProof)
   let
-    t :: Tree Int
-    t = number $ emptyTree 2 ()
+    t :: [Tree Int]
+    t = number [emptyTree 2 ()]
     delPath = intWToIntInt $ fromJust $ goIdxToHaskellPath 2 (fst $ getRootsReverse 4 2) 0 -- (0, [0,0])
       where
       intWToIntInt :: (Int, [Word64]) -> (Int, [Int])
       intWToIntInt (x, y) = (fromIntegral x, map fromIntegral y)
-  deleted <- prog [t] delPath putStrLn
+  deleted <- prog t [delPath] traceM
   print deleted
--- c/RemtransTest.hs
+++ c/RemtransTest.hs
@@ -2,15 +2,17 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import           Hedgehog ((===), annotateShow, annotate, Gen)
+import Data.Maybe
+import System.Exit (die)
+import           Hedgehog ((===), annotate, annotateShow, Gen)
 import qualified Hedgehog
 import qualified Hedgehog.Gen as Gen
---import qualified Hedgehog.Range as Range
+import qualified Hedgehog.Range as Range
 
 import Control.Monad
+import Data.Set hiding (map)
 
-import Remtrans (number, Tree(..), prog, leavesOnly, findInForest)
-import qualified Remtrans
+import Remtrans (number, Tree(..), prog, leavesOnly, findInForest, bitsSet)
 
 tree :: Gen (Tree ())
 tree = Gen.recursive Gen.choice [
@@ -19,32 +21,40 @@ tree = Gen.recursive Gen.choice [
     Gen.subterm tree (\subtree -> Node () subtree subtree)
   ]
   
-
-prop_removesNotTooMuch :: Hedgehog.Property
-prop_removesNotTooMuch =
-  Hedgehog.property $ do
-    (treeSet :: Tree ()) <- Hedgehog.forAll tree
-    let numbered :: (Tree Int) = number treeSet
-    vals :: [Int] <- Hedgehog.forAll $ Gen.shuffle $ leavesOnly numbered
+deleteTwo :: Hedgehog.PropertyT IO (Int, Int, [Tree (Maybe Int)])
+deleteTwo = do
+    treeSet :: Set (Tree ()) <- Hedgehog.forAll $ Gen.set (Range.linear 1 100) tree
+    let numbered :: [Tree Int] = number $ toList treeSet
+    vals :: [Int] <- Hedgehog.forAll $ Gen.shuffle $ join $ map leavesOnly numbered
     unless (length vals >= 2) Hedgehog.discard
     let toKeep : toDelete : _ = vals
     unless (toKeep /= toDelete) Hedgehog.discard
     let fun (_,Node v _ _) | v == toDelete = Just ()
         fun _ = Nothing
-    let Just path = findInForest fun [numbered]
+    let Just path = findInForest fun numbered
     annotateShow path
-    deleted <- prog [numbered] path annotateShow
-    True === (any (toKeep `elem`) deleted)
+    deleted <- prog numbered [path] annotate
+    fmap length deleted === fmap (length.(fmap fromJust)) deleted
+    return (toKeep, toDelete, deleted)
 
---prop_removesAskedElem :: Hedgehog.Property
---prop_removesAskedElem =
---  Hedgehog.property $ do
---    undefined
+prop_removesNotTooMuch :: Hedgehog.Property
+prop_removesNotTooMuch =
+  Hedgehog.property $ do
+    (toKeep, _, deleted) <- deleteTwo
+    True === (any ((Just toKeep) `elem`) deleted)
+
+prop_removesAskedElem :: Hedgehog.Property
+prop_removesAskedElem =
+  Hedgehog.property $ do
+    (_, toDelete, deleted) <- deleteTwo
+    False === (any ((Just toDelete) `elem`) deleted)
 
 li :: [(Hedgehog.PropertyName, Hedgehog.Property)]
 li = [  ("prop_removesNotTooMuch", prop_removesNotTooMuch)
---     ,  ("prop_removesAskedElem",  prop_removesAskedElem)
+     ,  ("prop_removesAskedElem",  prop_removesAskedElem)
      ]
 
-main = Hedgehog.checkSequential $ Hedgehog.Group "property tests" li
+main = do
+  unless (2 == (length $ bitsSet (3 :: Int))) (die "bad bitsSet")
+  Hedgehog.checkSequential $ Hedgehog.Group "property tests" li
 --main = Remtrans.main
